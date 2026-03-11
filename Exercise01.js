8f4eef9480339ad737674e52ff11f3dc2e7aaefd
Alejo <alejoricoparamo211@gmail.com>
2026-03-11 13:38:15 -0500

    Mi primer commit

---
+++ Exercise01.js
@@ -0,0 +1,18 @@
+//crear usuario 
+
+let ingresaruser = prompt("Crea un usuario")
+
+//crear password
+
+let crearpassword = prompt("Hola, Crea tu contraseña")
+
+//reingresar passowrd
+
+let verificarpassword = prompt("Ingresa por favor tu contraseñade nuevo")
+
+//verificar password
+
+
+
+
+
